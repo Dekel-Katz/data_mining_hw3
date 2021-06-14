401616c2f05b3782b76fbd00854b757f8a0dead8
Avigail <iggiebaru@gmail.com>
2021-06-14 22:19:31 +0300

    finished all questions - needs organaziing

--- tokenize_book.py
+++ tokenize_book.py
@@ -7,6 +7,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import spacy
+import re
 
 
 SIGNS = ['.', ',', ':', ';', '!', '?', '*', '#', '@', '&', '(', ')',
@@ -95,54 +96,16 @@ def book_stemming(words_list):
     return words_list
 
 
-def visualize_doc_ranks(tokens_dict, all_words_count):
+def visualize_doc_ranks(tokens_dict):
     tokens = [key for key in tokens_dict.keys()]
     ranks = list(range(1, len(tokens_dict)+1))
-    frequencies = [math.log(value / all_words_count, 2) for value in
+    frequencies = [math.log(value, 2) for value in
                    tokens_dict.values()]
     plt.title("words ranks and frequencies in 'around the wold in 80 days'")
     plt.plot(ranks, frequencies)
     plt.xlabel('Ranks')
     plt.ylabel('Frequencies')
     plt.show()
-
-
-def extract_adj_plus_noun(words):
-    adjective = ['JJ', 'JJS', 'JJR']
-    noun = ['NN', 'NNS', 'NNP', 'NNPS']
-    word_types = nltk.pos_tag(words)
-    print(word_types)
-    phrase_list = []
-    for index in range(len(word_types)):
-        if word_types[index][1] in adjective:
-            if word_types[index+1][1] in noun:
-                phrase = word_types[index][0] + ' ' + word_types[index+1][0]
-                adj_index = index - 1
-                adj_list = []
-                while True:
-                    if word_types[adj_index][1] in adjective:
-                        adj_list.append(word_types[adj_index][0])
-                        adj_index -= 1
-                        if adj_index < 0:
-                            break
-                    else:
-                        break
-                for adj in adj_list[0::-1]:
-                    phrase = adj + ' ' + phrase
-                noun_list = []
-                noun_index = index + 1
-                while True:
-                    if word_types[noun_index][1] in noun:
-                        noun_list.append(word_types[noun_index][0])
-                        noun_index += 1
-                        if noun_index == len(word_types):
-                            break
-                    else:
-                        break
-                for no in noun_list[1:]:
-                    phrase = phrase + ' ' + no
-                phrase_list.append(phrase)
-    return phrase_list
 
 
 def spacy_extract_adj_plus_noun(words):
@@ -182,6 +145,26 @@ def top_20_phrases(spacy_tokenized):
         top_20[key] = val
         counter +=1
 
+######################### G #########################
+def create_word_cloud_words(words):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(words)
+    word_cloud_string = ""
+    for word in doc:
+        if word.pos_ == "PROPN" and word.text not in SIGNS:
+            word_cloud_string = word_cloud_string + word.text + " "
+    return word_cloud_string
+######################### G #########################
+
+######################### H #########################
+def find_consecutive_words(words):
+    consecutive_phrases = []
+    consecutive_words = re.findall(r"\b([A-Za-z]+)\s\1\b", words.lower())
+    for word in consecutive_words:
+        consecutive_phrases.append(word + " " + word)
+    return consecutive_phrases
+######################### H #########################
+
 
 if __name__ == '__main__':
     text_file = 'around_the_world_in_eighty_days.txt'
@@ -190,17 +173,19 @@ if __name__ == '__main__':
     # no_stops_list = remove_stopwords(words)
     # stemm_list = book_stemming(no_stops_list)
     # stemmed_d = create_token_dict(stemm_list)
-    spacy_phrases = spacy_extract_adj_plus_noun(clean_text)
-    tokenized_spacy = create_token_dict(spacy_phrases)
-    visualize_doc_ranks(top_20_phrases(tokenized_spacy), len(spacy_phrases))
-        
-
+    # spacy_phrases = spacy_extract_adj_plus_noun(clean_text)
+    # tokenized_spacy = create_token_dict(spacy_phrases)
+    # visualize_doc_ranks(top_20_phrases(tokenized_spacy))
     # test = "google. Sweet peanut butter cookies. Peanut butter jelly " \
     #        "sandwich. John is sitting in the oval office."
-    # visualize_doc_ranks(stemmed_d, len(stemm_list))
+    # visualize_doc_ranks(stemmed_d)
     # d = create_token_dict(words)
     # d_no_stops = create_token_dict(no_stops_list)
-    # visualize_doc_ranks(d, len(words))
-    # visualize_doc_ranks(d_no_stops, len(no_stops_list))
+    # visualize_doc_ranks(d)
+    # visualize_doc_ranks(d_no_stops)
+    # ordered from here down:
 
-
+    ######################### G #########################
+    cloud_propn_lst = create_word_cloud_words(clean_text)
+    ######################### H #########################
+    print(find_consecutive_words(clean_text))
